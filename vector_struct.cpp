e8812bcc9dc0cb9992e24c0df3301da8cbb3f875
ILYJNY <crimssel@gmail.com>
2024-03-23 13:50:05 +0900

    for the test

---
+++ vector_struct.cpp
@@ -0,0 +1,14 @@
+//
+// Created by Backup on 2024-03-21.
+//
+#include "vector_struct.h"
+
+
+
+
+dimensions main_0(dimensions thing) {
+    double** dimensions_admin;
+
+
+
+}
---
+++ vector_struct.h
@@ -0,0 +1,23 @@
+//
+// Created by Backup on 2024-03-21.
+//
+
+#ifndef UNTITLED2_VECTOR_STRUCT_H
+#define UNTITLED2_VECTOR_STRUCT_H
+
+struct dimensions {
+    int dimension;
+    double* vectors;
+};
+
+
+
+
+
+dimensions main_0(dimensions thing);
+
+
+
+#endif //UNTITLED2_VECTOR_STRUCT_H
+
+
