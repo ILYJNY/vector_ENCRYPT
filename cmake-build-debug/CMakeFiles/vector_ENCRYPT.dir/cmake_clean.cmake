d42c9ec424bba87b0dfa7172731de6cfd812f121
ILYJNY <crimssel@gmail.com>
2024-05-03 01:09:28 +0900

    Making it

---
+++ cmake-build-debug/CMakeFiles/vector_ENCRYPT.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/vector_ENCRYPT.dir/main.cpp.obj"
+  "CMakeFiles/vector_ENCRYPT.dir/main.cpp.obj.d"
+  "libvector_ENCRYPT.dll.a"
+  "vector_ENCRYPT.exe"
+  "vector_ENCRYPT.exe.manifest"
+  "vector_ENCRYPT.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/vector_ENCRYPT.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main/2-d/2-dmain.h
@@ -0,0 +1,13 @@
+//
+// Created by Administrator on 2024-05-03.
+//
+#include "vector"
+#ifndef VECCTOR_ENCRYPT_2_DMAIN_H
+#define VECCTOR_ENCRYPT_2_DMAIN_H
+
+struct key {
+    std::vector<long double> first;
+    std::vector<long double> second;
+};
+
+#endif //VECCTOR_ENCRYPT_2_DMAIN_H
